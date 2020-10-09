c350a1d700c68e835261a487d8ab641474861f2f
Nicole Creange <48766292+nccreang@users.noreply.github.com>
2020-10-09 16:42:46 -0400

    typo

--- BGlib/trKPFM/analysis/__init__.py
+++ BGlib/trKPFM/analysis/__init__.py
@@ -1,7 +1,7 @@
 """
 for analysis
 """
-from . linear_tr_kpfm import  trKPFM_L_Analyzer
+from . linear_tr_kpfm import trKPFM_L_Analyzer
 from . spectral_trkpfm import trKPFM_S_Analyzer
 
 __all__ = ['linear_tr_kpfm','spectral_trkpfm']
