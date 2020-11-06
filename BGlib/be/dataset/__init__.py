57442e75b299f315e9f8a376e91deb2097cd438f
Rama Vasudevan <rama87@gmail.com>
2020-11-06 18:09:39 -0500

    cleaning up the RawBE classes a bit

--- BGlib/be/dataset/__init__.py
+++ BGlib/be/dataset/__init__.py
@@ -1,5 +1,6 @@
 from . import be_raw_dataset
 from . import be_sho_dataset
 from . import ckpfm_dataset
-
-__all__ = ['be_raw_dataset', 'be_sho_dataset', 'ckpfm_dataset']
+from be_raw_datasets import RawBEDataset, RawBELINEDataset, RawBEPSDataset
+__all__ = ['be_raw_dataset', 'be_sho_dataset', 'ckpfm_dataset', 'RawBEDataset',
+           'RawBEPSDataset', 'RawBELINEDataset']
