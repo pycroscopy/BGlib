2f9af750a14c43dcb38c29be1e2a6627df68a7a8
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-11-11 14:13:58 -0500

    swapped the import statement to sidpy

--- BGlib/be/translators/beps_data_generator.py
+++ BGlib/be/translators/beps_data_generator.py
@@ -15,8 +15,8 @@ from sidpy.sid import Translator
 from pyUSID.io.hdf_utils import link_as_main, copy_dataset, \
     write_main_dataset, create_indexed_group, create_results_group, \
     write_reduced_anc_dsets
-
 from sidpy.hdf.reg_ref import copy_all_region_refs , write_region_references
+
 from pyUSID.io.write_utils import Dimension, calc_chunks
 from pyUSID.io.image import read_image
 
