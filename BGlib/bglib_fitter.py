a2b95574adae0cff66411e92bd0d4ea350b72963
nccreang <nccreang@ncsu.edu>
2021-05-07 16:56:00 -0400

    change imports

--- BGlib/bglib_fitter.py
+++ BGlib/bglib_fitter.py
@@ -1,8 +1,8 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-# import bglib_process
-# import bglib_guesser
+from bglib_process import BGlibProcess
+from bglib_guesser import *
 from sklearn.metrics import r2_score
 from scipy.optimize import curve_fit
 
