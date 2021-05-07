7ba2795bc4bc1706ad870131ee850729373832b1
nccreang <nccreang@ncsu.edu>
2021-05-07 16:45:25 -0400

    refactor directories and __init__

--- BGlib/__init__.py
+++ BGlib/__init__.py
@@ -9,5 +9,7 @@ Submodules
 """
 from .__version__ import version as __version__
 from . import be, gmode, trKPFM
+from . import bglib_fitter, bglib_guesser, bglib_process
 
-__all__ = ['__version__', 'be', 'gmode', 'trKPFM']
+__all__ = ['__version__', 'be', 'gmode', 'trKPFM', 'bglib_guesser', 'bglib_fitter', 'bglib_process']
+
--- BGlib/be/__init__.py
+++ BGlib/be/__init__.py
@@ -14,6 +14,6 @@ Submodules
 """
 
 from . import analysis, translators, viz
-from . import bglib_fitter, bglib_guesser, bglib_process
 
-__all__ = ['analysis', 'translators', 'viz', 'bglib_guesser', 'bglib_fitter', 'bglib_process']
+
+__all__ = ['analysis', 'translators', 'viz']
