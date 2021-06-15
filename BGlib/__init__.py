bbad663ed5189c4172f668402160c1d19b514671
nccreang <nccreang@ncsu.edu>
2021-06-15 12:26:21 -0400

    temp removing trKPFM library

--- BGlib/__init__.py
+++ BGlib/__init__.py
@@ -8,7 +8,7 @@ Submodules
     :toctree: _autosummary
 """
 from .__version__ import version as __version__
-from . import be, gmode, trKPFM
+from . import be, gmode#, trKPFM
 
-__all__ = ['__version__', 'be', 'gmode', 'trKPFM']
+__all__ = ['__version__', 'be', 'gmode']#, 'trKPFM']
 
