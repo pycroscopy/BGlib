aec4f3985c67489b2d867b0303044b73d8447a61
nccreang <nccreang@ncsu.edu>
2021-07-23 16:43:58 -0400

    making unit tests for BELoopFitter

--- BGlib/tests/be/analysis/test_BELoopFitter.py
+++ BGlib/tests/be/analysis/test_BELoopFitter.py
@@ -6,45 +6,164 @@ import sys
 from BGlib.be.analysis.BELoopFitter import *
 
 class TestFitFunc(unittest.TestCase):
-
-    def test_fit_fun_inputs(self):
+    """
+    Tests for fit_func in BELoopFitter
+    test for:
+    fit_func -> TODO: roll, curve_fit
+    format_xvec -> TODO
+    calc_priors -> TODO
+    calc_mean_fit -> TODO
+    fit_parallel -> TODO
+    fit_series -> TODO
+    convert_coeff2loop -> TODO
+    loop_fit_func2 -> TODO
+    """
+    def test_fit_func_inputs(self):
         arr1 = np.asarray([1,3,5])
         arr2 = np.asarray([7,11,13])
         p0 = np.random.normal(0.1, 5, 9)
         self.assertTrue(np.allclose(arr1,arr2,p0,fit_func(arr1,arr2,p0)))
 
     def test_invalid_values1(self):
+        """
+        input arrays cannot be strings
+        """
         with self.assertRaises(TypeError):
             _ = fit_func('array','array2','values') #test strings
 
     def test_invalid_values2(self):
+        """
+        Input arrays should have len() > 1
+        """
         with self.assertRaises(TypeError):
             _ = fit_func(0,1,3) # test integers
     def test_invalid_values3(self):
+        """
+        Input arrays cannot be lists, should be np.array
+        """
         with self.assertRaises(TypeError):
             _ = fit_func([0,1,3], [5,7,11], [13,17,23]) #test lists
 
-    def test_format_xvec(self):
-        return
+class TestFormatXvec(unittest.TestCase):
+    def test_format_xvec_input(self):
+        """
+        Array should be longer than length 1
+        """
+        arr1 = np.asarray([1])
+        with self.assertRaises(TypeError):
+            _ = format_xvec(arr1)
 
-    def test_calc_priors(self):
-        return
+    def test_format_xvec_input2(self):
+        """
+        Array should be a numeric array
+        """
+        arr1 = np.asarray(['test','test2'])
+        with self.assertRaises(TypeError):
+            _ = format_xvec(arr1)
 
-    def test_calc_mean_fit(self):
-        return
+    def test_max(self):
+        """
+        Testing array max is correct
+        """
+        arr1 = np.asarray([1,2,3,4,5,4,3,2,1])
+        max_x = np.where(xvec == np.max(arr1))[0]
+        expected = 4
+        self.assertEqual(expected,max_x)
+#TODO: need to test for if max is an integer?  I would think it should be fine
 
+class TestCalcPriors(unittest.TestCase):
+    def test_calc_priors_input(self):
+        """
+        Input should be a np.array not a list
+        """
+        arr1 = [1,2,3,4]
+        with self.assertRaises(TypeError):
+            _ = calc_priors(arr1)
+
+    def test_calc_priors_input2(self):
+        """
+        Input should not contain strings
+        """
+        arr1 = ['test',2,3,4]
+        with self.assertRaises(TypeError):
+            _ = calc_priors(arr1)
+
+    def test_calc_priors_input3(self):
+        """
+        Input should be longer than 1 element
+        """
+        arr1 = np.asarray([1])
+        with self.assertRaises(TypeError):
+            _ = calc_priors(arr1)
+
+
+class TestCalcMeanFit(unittest.TestCase):
+    def test_input(self):
+        """
+        Input should be a np.array not a list
+        """
+        arr1 = [1, 2, 3, 4]
+        with self.assertRaises(TypeError):
+            _ = calc_mean_fit(arr1)
+
+    def test_input2(self):
+        """
+        Input should not contain strings
+        """
+        arr1 = ['test', 2, 3, 4]
+        with self.assertRaises(TypeError):
+            _ = calc_mean_fit(arr1)
+
+    def test_input3(self):
+        """
+        Input should be longer than 1 element
+        """
+        arr1 = np.asarray([1])
+        with self.assertRaises(TypeError):
+            _ = calc_mean_fit(arr1)
+#TODO: need to make tests for basic functions like append, curve_fit, @, deepcopy, argmin?
+
+class TestFitParallel(unittest.TestCase): #TODO
     def test_fit_parallel(self):
         return
 
+class TestFitSeries(unittest.TestCase): #TODO
     def test_fit_series(self):
         return
 
+class TestConvertCoeff2loop(unittest.TestCase): #TODO
     def test_convert_coeff2loop(self):
         return
 
-    def test_loop_fit_func2(self):
-        return
+class TestLoopFitFunc(unittest.TestCase):
+    def test_fit_func_inputs(self):
+        arr1 = np.asarray([1, 3, 5])
+        arr2 = np.asarray([7, 11, 13])
+        p0 = np.random.normal(0.1, 5, 9)
+        self.assertTrue(np.allclose(arr1, arr2, p0, loop_fit_func2(arr1, arr2, p0)))
+
+    def test_invalid_values1(self):
+        """
+        input arrays cannot be strings
+        """
+        with self.assertRaises(TypeError):
+            _ = loop_fit_func2('array', 'array2', 'values')  # test strings
+
+    def test_invalid_values2(self):
+        """
+        Input arrays should have len() > 1
+        """
+        with self.assertRaises(TypeError):
+            _ = loop_fit_func2(0, 1, 3)  # test integers
+
+    def test_invalid_values3(self):
+        """
+        Input arrays cannot be lists, should be np.array
+        """
+        with self.assertRaises(TypeError):
+            _ = loop_fit_func2([0, 1, 3], [5, 7, 11], [13, 17, 23])  # test lists
 
 
 
-
+if __name__ == '__main__':
+    unittest.main()
